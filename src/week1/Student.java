eb2e764c8d9827d4a9224c2eb9b3b6a412a3c9d0
levanhuong17020825 <43227882+levanhuong17020825@users.noreply.github.com>
2018-10-04 09:38:27 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -9,8 +9,16 @@ public class Student {
     /**
      * Constructor 1
      */
+    private String name;
+    private String id;
+    private String group;
+    private String email;
+    
     Student() {
         // TODO:
+        name = "Student";
+        id = "000";
+        email = "uet@vnu.edu.vn";
     }
 
     /**
@@ -21,6 +29,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        name = n;
+        id = sid;
+        email = em;
+        group = "INT22041";
     }
 
     /**
@@ -29,10 +41,54 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        name = s.name;
+        id = s.id;
+        group = s.group;
+        email = s.email;
+    }
+    //name
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //ID
+    public void setID(String id_) {
+        this.id = id_;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    //Group
+    public void setGroup(String g) {
+        this.group = g;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    //Email
+    public void setEmail(String e) {
+        this.email = e;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     String getInfo() {
         // TODO:
+        System.out.printf("%-20s \t", getName());
+        System.out.printf("%-10s \t", getID());
+        System.out.printf("%-10s \t", getGroup());
+        System.out.printf("%s", getEmail());
+        System.out.println("");
         return null; // xóa dòng này sau khi cài đặt
     }
 }
