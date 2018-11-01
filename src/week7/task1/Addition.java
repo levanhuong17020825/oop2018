64a1532d01e50568197a57580bc78de1abccb27f
levanhuong17020825 <43227882+levanhuong17020825@users.noreply.github.com>
2018-11-01 10:42:45 +0700

    Update Addition.java

--- src/week7/task1/Addition.java
+++ src/week7/task1/Addition.java
@@ -1,4 +1,23 @@
 package week7.task1;
 
 public class Addition {
+  public class Addition extends BinaryExpression {
+     private Expression left;
+    private Expression right;
+     public Addition(Expression firstExpression, Expression secondExpression){
+        left = firstExpression;
+        right = secondExpression;
+    }
+     @Override
+    public Expression left() {
+        return left;
+    }
+     @Override
+    public Expression right() {
+        return right;
+    }
+     @Override
+    public int evaluate() {
+        return left.evaluate() + right.evaluate();
+    }
 }
