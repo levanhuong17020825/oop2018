2d3cf0553e8a43786cb8ba66de5d65667170a7f6
levanhuong17020825 <43227882+levanhuong17020825@users.noreply.github.com>
2018-11-01 10:46:04 +0700

    Update Square.java

--- src/week7/task1/Square.java
+++ src/week7/task1/Square.java
@@ -1,4 +1,18 @@
 package week7.task1;
 
 public class Square {
+  public class Square extends Expression {
+     private Expression expression;
+     public Square(Expression expression){
+        this.expression = expression;
+    }
+     @Override
+    public String toString() {
+        return null;
+    }
+     @Override
+    public int evaluate() {
+        int expressionValue = this.expression.evaluate();
+        return expressionValue*expressionValue;
+    }
 }
